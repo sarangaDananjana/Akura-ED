dcc5f5334c76eacdf7bba4bf20388f5205964f1a
sarangaDananjana <dananjanasaranga@gmail.com>
2026-05-28 01:12:22 +0530

    updated

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """Admin configuration for CustomUser with all custom fields visible."""
+
+    list_display = ('username', 'email', 'is_premium', 'daily_flashcard_limit', 'phone_number', 'is_staff', 'is_active')
+    list_filter = ('is_premium', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'phone_number')
+
+    # Add custom fields to the existing UserAdmin fieldsets
+    fieldsets = UserAdmin.fieldsets + (
+        ('Akura Profile', {
+            'fields': ('is_premium', 'daily_flashcard_limit', 'phone_number', 'auth_version'),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Akura Profile', {
+            'fields': ('is_premium', 'daily_flashcard_limit', 'phone_number'),
+        }),
+    )
+
+    readonly_fields = ('auth_version',)
